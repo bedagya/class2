8a639cf195dddef6a0554a8a34f061d7f514cdef
Bedagya <bedagya123@gmail.com>
2020-02-23 00:02:28 +0545

    hw

--- properties.js
+++ properties.js
@@ -1,16 +1,12 @@
-var getKeys = function(obj){
+function _keys(obj){
+    if (!isObject(obj)) return [];
+    if (Object.keys) return Object.keys(obj);
     var keys = [];
-    for(var key in obj){
-       keys.push(key);
-    }
+    for (var key in obj) if (_.has(obj, key)) keys.push(key);
     return keys;
- }
+  }
 function isObject(obj){
-    var student={
-        name:"David Rayy",
-        sclass:"VI",
-        rollno:12 };
-    }
-console.log(_keys({name, sclass, rollno}));
-
-
+    var type = typeof obj;
+    return type === 'function' || type === 'object' && !!obj;
+  }
+console.log(_keys({name:"David Royy",sclass:"VI",rollno:12}));
